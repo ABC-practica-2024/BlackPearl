76fb88f16354f9887e2e22f4850a3db43ac88e97
Darius <dariusmecea@gmail.com>
2024-08-27 15:29:08 +0300

    feat: Connect ESP32 to MQTT broker on Raspberry Pi

---
+++ Firmware/ESP32C6_RaspberryPi_Connection/Client_ESP/Client_ESP.ino
@@ -0,0 +1,116 @@
+/*********
+  Rodolphe Beloncle
+  Complete project details at https://github.com/RodolpheBeloncle/IOT-Projects
+*********/
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+
+// Replace the next variables with your SSID/Password combination
+const char* ssid = "Mateinfo";
+const char* password = "computer";
+
+// Add your MQTT Broker IP address, example:
+const char* mqtt_server = "172.30.245.94";
+
+// Add your MQTT Broker username and password
+const char* mqtt_username = "telecomanda";
+const char* mqtt_password = "ABCabc";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+// LED Pin
+const int ledPin = 3;
+
+void setup() {
+  Serial.begin(921600);
+  delay(2000);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
+  pinMode(ledPin, OUTPUT);
+}
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+  Serial.println(messageTemp);
+  Serial.println("------------------");
+
+  // Control GPIOs with MQTT
+  if (String(topic) == "motor/forward") {
+    Serial.print("Changing output to ");
+    if(messageTemp == "on"){
+      Serial.println("on");
+      digitalWrite(ledPin, HIGH);
+    }
+    else if(messageTemp == "off"){
+      Serial.println("off");
+      digitalWrite(ledPin, LOW);
+    }
+  }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect with username and password
+    if (client.connect("ESP8266Client", mqtt_username, mqtt_password)) {
+      Serial.println("connected");
+      // Subscribe
+      client.subscribe("motor/forward");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 5000) {
+    lastMsg = now;
+  }
+}
